b4777d2c1318d3d02fb552c03eb8c8a3de05188a
bilhelm96 <bilhelm96@gmail.com>
2022-11-01 18:37:31 -0400

    Logging code and relay command update

--- src/DEV_Fireplace.h
+++ src/DEV_Fireplace.h
@@ -8,28 +8,43 @@ struct DEV_Fireplace : Service::Switch {
     SpanCharacteristic *power;
 
   DEV_Fireplace(uint8_t switchPin, byte addr) : Service::Switch() {
-    power = new Characteristic::On(0, true);
+    power = new Characteristic::On(false, true);
     Wire.begin();
     relay.begin(addr);
     this->switchPin = switchPin;
+
+    Serial.print(F("Initializing Fireplace Controller\n"));
+    Serial.print(F("Power Relay on I2C address: "));
+    Serial.print(addr);
+    Serial.print("\n");
+    Serial.print(F("Power switch on pin: "));
+    Serial.print(switchPin);
+    Serial.print("\n");
+    Serial.print(F("Initial power state: "));
+    Serial.print(power->getVal());
+    Serial.print("\n");
     
     new SpanButton(switchPin);
-    power->setVal(1-power->getVal());
+    relay.commandRelay(power->getVal());
   }
 
   boolean update() {
-    if (power->getNewVal()) {
-      relay.turnRelayOn();
-    }
-    else {
-      relay.turnRelayOff();
-    }
+    LOG1(F("In update function new power state: "));
+    LOG1(power->getNewVal());
+    LOG1("\n");
+
+    relay.commandRelay(power->getNewVal());
     return true;
   }
 
   void button(int pin, int pressType) override {
     if (pin == switchPin) {
-      power->setVal(1-power->getVal());
+      LOG1(F("Power swich pressed\n"));
+      LOG1(F("New power state: "));
+      LOG1(!(power->getVal()));
+      LOG1("\n");
+      power->setVal(!(power->getVal()));
+      relay.commandRelay(power->getVal());
     }
   }
 };
